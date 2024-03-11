da03d8a8f13acb86b3c25f9a5ca3618b7664d538
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-03-11 21:20:26 +0900

    added animation to button and add page

---
+++ travel/src/components/intro/IntroButton.tsx
@@ -0,0 +1,37 @@
+import { Center } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function IntroButton({ words }: { words: string }) {
+    const navigate = useNavigate();
+    const [opacity, setOpacity] = useState(0);
+
+    useEffect(() => {
+        const timeout = words.split(" ").length * 0.2;
+        setTimeout(() => {
+            setOpacity(1);
+        }, timeout * 1000 + 2000);
+    }, []);
+
+    return (
+        <Center
+            opacity={opacity}
+            mt="30px"
+            fontSize="25px"
+            fontWeight="bold"
+            px="20px"
+            py="15px"
+            bgColor="black"
+            borderRadius="30px"
+            _hover={{
+                cursor: "pointer",
+                color: "black",
+                bgColor: "white",
+            }}
+            transition="all 0.3s linear"
+            onClick={() => navigate("/add")}
+        >
+            Let's explore your memory
+        </Center>
+    );
+}
--- travel/src/pages/Intro.tsx
+++ travel/src/pages/Intro.tsx
@@ -1,32 +1,20 @@
 import { Center, VStack } from "@chakra-ui/react";
 import { IntroText } from "../components/intro/IntroText";
 import { Helmet } from "react-helmet";
+import IntroButton from "../components/intro/IntroButton";
 
 export default function Intro() {
+    const comment =
+        "Travel can expand our thoughts and experiences. Is there a country that stands out in your memory or a country you really want to visit? If you have a special memory, think about it again.";
+
     return (
         <Center w="100%" h="100vh">
             <Helmet>
                 <title>Intro</title>
             </Helmet>
             <VStack>
-                <IntroText words="Travel can expand our thoughts and experiences. Is there a country that stands out in your memory or a country you really want to visit? If you have a special memory, think about it again." />
-                <Center
-                    mt="30px"
-                    fontSize="25px"
-                    fontWeight="bold"
-                    px="20px"
-                    py="15px"
-                    bgColor="black"
-                    borderRadius="30px"
-                    _hover={{
-                        cursor: "pointer",
-                        color: "black",
-                        bgColor: "white",
-                    }}
-                    transition="all 0.3s linear"
-                >
-                    Let's explore your memory
-                </Center>
+                <IntroText words={comment} />
+                <IntroButton words={comment} />
             </VStack>
         </Center>
     );
